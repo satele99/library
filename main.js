3a1fbf1e809e2f8bbb69434a079fbc24e65454ea
Amir Ali <satele.works@gmail.com>
2023-01-14 03:52:42 -0500

    working on styling the book cards and adding them dynamically to the page

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
+const books = [
+    {
+        'title': 'The Wheel Of Time: Eye Of The World',
+        'pages': 782,
+        'author' : 'Robert Jordan',
+        'read': 'yes'
+    }
+];
+const formBtn = document.getElementById('form-btn');
+
+formBtn.addEventListener('click', () => {
+    console.log('hello')
+});
+
 
 function on() {
     let overlay = document.getElementById('overlay');
     overlay.style.display = 'block';
 };
+
+// function createBook() {
+//     console.log('added')
+//     const addBookContainer = document.getElementById('add-book');
+//     const cardDiv = document.createElement('div');
+
+//     addBookContainer.appendChild(cardDiv);
+//     cardDiv.classList = 'book-card';
+//     cardDiv.innerText = 'New Book';
+// };

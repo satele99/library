819e1422caaf969d50f4f77f448ca8d928bddcb9
Amir Ali <satele.works@gmail.com>
2023-01-15 01:00:03 -0500

    polished form, its working

--- main.js
+++ main.js
@@ -12,6 +12,11 @@ const overlayRemove = document.getElementById('overlay-remove');
 const addBookbtn = document.getElementById('add-book-btn');
 const formBtn = document.getElementById('form-btn');
 
+const title = document.getElementById('title').value;
+const author = document.getElementById('author').value;
+const pages = document.getElementById('pages').value;
+const read = document.getElementById('read');
+
 formBtn.addEventListener('click', (event) => {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
@@ -20,15 +25,15 @@ formBtn.addEventListener('click', (event) => {
     const validation = overlayForm.checkValidity();
     const validationReport = overlayForm.reportValidity();
     event.preventDefault();
+
     if(validation == true){
         overlay.style.display = 'none';
-        
         const addBookContainer = document.getElementById('add-book');
         const cardDiv = document.createElement('div');
-        
         addBookContainer.appendChild(cardDiv);
         cardDiv.classList = 'book-card';
         cardDiv.innerText = title+' '+author+' '+pages;
+        overlayForm.reset();
     };
 });
 
